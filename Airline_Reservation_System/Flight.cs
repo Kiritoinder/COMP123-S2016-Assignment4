9bb542ba5d0f3e54117693bd5b5189692dd03f9d
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 14:02:02 -0400

    Added Airline_Reservation_System project, Flight Class, Created a menu.

---
+++ Airline_Reservation_System/Flight.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Reservation_System
+{
+    class Flight
+    {
+        private bool[] _seatsAvailible = new bool[10];
+        private string ChangeClass;
+
+        //Public Properties ++++++++++++++++++++++++++++
+        public Flight()
+        {
+            _initializeArray();
+        }
+        //Private Methdos ++++++++++++++++++++++++++++++
+        private void _initializeArray()
+        {
+            for (int i = 0; i < _seatsAvailible.Length; i++)
+            {
+                this._seatsAvailible[i] = false;
+            }
+        }
+
+        //Public Methods +++++++++++++++++++++++++++++++
+        public void FirstClass()
+        {
+            int seatNumber = 0;
+            while (seatNumber < 5)
+            {
+                if (_seatsAvailible[seatNumber] == false)
+                {
+                    this._seatsAvailible[seatNumber] = true;
+                    this.viewSeats();
+                    break;
+
+                }
+                else if (seatNumber == 4)
+                {
+                    Console.WriteLine("All first class seats have been booked");
+                    Console.WriteLine("Whould you like to book your seat in the Economic Class?/n Y) Yes /n N) No");
+                    ChangeClass = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Next flight leaves in 3 hours");
+                }
+                if (ChangeClass == "Y")
+                {
+                    EconomicClass();
+                }
+                else
+                {
+
+                }
+                seatNumber++;
+            }
+        }
+
+        public void EconomicClass()
+        {
+            int seatNumber = 5;
+            while (seatNumber < 10)
+            {
+                if (_seatsAvailible[seatNumber] == false)
+                {
+                    this._seatsAvailible[seatNumber] = true;
+                    this.printEconomicclass();
+                    break;
+                }
+                else if (seatNumber == 9)
+                {
+                    Console.WriteLine("All Economic class Seats have been booked.");
+                    Console.WriteLine("Would you like to book your seat in First Class? Y) Yes  N) No");
+                    ChangeClass = Convert.ToString(Console.ReadLine());
+
+                }
+                if (ChangeClass == "Y")
+
+                    FirstClass();
+                else
+                {
+
+                }
+
+            }
+            seatNumber++;
+        }
+        public void viewSeats()
+        {
+            Console.WriteLine();
+            for (int index = 0; index < _seatsAvailible.Length; index++)
+            {
+                if (this._seatsAvailible[index] == true)
+                {
+                    Console.WriteLine("First Class Seat Booked");
+                }
+
+                else if (index > 5)
+                {
+                    Console.WriteLine("Economic Class Seat Booked");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------");
+        }
+        public void printEconomicclass()
+        {
+            for (int index = 0; index < _seatsAvailible.Length; index++)
+            {
+                if (this._seatsAvailible[index] == true)
+                {
+                    Console.Write(" Selected  ");
+                }
+
+            }
+        }
+        public void Exit()
+        {
+            Console.Clear();
+        }
+
+    }
+}
---
+++ Airline_Reservation_System/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Reservation_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int selection = 0;
+            Random random = new Random();
+            Flight lufthansa = new Flight();
+            while (selection != 3)
+            {
+                Console.WriteLine("|=====Lufthansa Airline=====");
+                Console.WriteLine("|1. For First Class         |");
+                Console.WriteLine("|2. For Economy Class       |");
+                Console.WriteLine("|3. Exit                    |");
+                Console.WriteLine("|===========================| \n");
+                Console.Write("Please select from above options: ");
+                try
+                {
+                    selection = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    selection = 0;
+                }
+                switch (selection)
+                {
+                    case 1:
+                        lufthansa.FirstClass();
+                        break;
+
+                    case 2:
+                        lufthansa.EconomicClass();
+                        break;
+                    case 3:
+                        lufthansa.Exit();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input");
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
