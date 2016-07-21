8b8f64499beae5b0cad598d75493249536f11f45
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 13:40:53 -0400

    Added Internal Documentation

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * Author: Inderjeet Singh
+ * StudentNumber: 300874118
+ * Description: Driver class for Dice Rolling Simulation
+ * Version: 0.0.1
+ * DateCreated: July 21rd, 2016
+ * DateModified: July 21rd, 2016
+ */
 namespace Assignment_4
 {
+    /**
+     * this class is the "driver" class for my program
+     * @class Program
+     */
     class Program
     {
+        /**Main method of driver class
+         * 
+         * @method Main
+         * param {string} args
+         */
         static void Main(string[] args)
         {
             int[] tally = new int[13];
@@ -16,6 +32,7 @@ namespace Assignment_4
 
             int presentNumber = 0;
 
+            // This loop calculates the sum of two numbers on dice
             for (int rolls = 1; rolls < 36000; rolls++)
             {
                 presentNumber = random.Next(1, 7) + random.Next(1, 7);
@@ -26,7 +43,8 @@ namespace Assignment_4
 
             Console.WriteLine("************************************");
             Console.WriteLine("Dice #   |    Number of times rolled");
-            //This loop outputs the sum of the two dice and the number of times the sum os got.
+            //This loop outputs the sum of the two # and the number of
+            // times the sum is repeated
             for (int Index = 2; Index < 13; ++Index)
             {
                 Console.WriteLine("{0,4}     | {1,17}", Index, tally[Index]);
@@ -34,5 +52,5 @@ namespace Assignment_4
             Console.WriteLine("*************************************");
             Console.ReadLine();
         }
-    }
-}
+    } // End of namespace
+}// End of program
