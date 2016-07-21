e59dd50ddde11e21bf14dadb5cfa43776487da2c
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 13:29:34 -0400

    Initial Commit

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -10,6 +10,19 @@ namespace Assignment_4
     {
         static void Main(string[] args)
         {
+            int[] tally = new int[13];
+
+            Random random = new Random();
+
+            int presentNumber = 0;
+
+            for (int rolls = 1; rolls < 36000; rolls++)
+            {
+                presentNumber = random.Next(1, 7) + random.Next(1, 7);
+
+                ++tally[presentNumber];
+            }
+
         }
     }
 }
