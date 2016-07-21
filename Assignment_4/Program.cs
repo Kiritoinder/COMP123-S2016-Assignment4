3a866a2b1f01a70de120864d8fc53f91af5333ba
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 13:31:55 -0400

    Added the sum of two numbers on dice

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -22,7 +22,17 @@ namespace Assignment_4
 
                 ++tally[presentNumber];
             }
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine("************************************");
+            Console.WriteLine("Dice #   |    Number of times rolled");
+            //This loop outputs the sum of the two dice and the number of times the sum os got.
+            for (int Index = 2; Index < 13; ++Index)
+            {
+                Console.WriteLine("{0,4}     | {1,17}", Index, tally[Index]);
+            }
+            Console.WriteLine("*************************************");
+            Console.ReadLine();
         }
     }
 }
